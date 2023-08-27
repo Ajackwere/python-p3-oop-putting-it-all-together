86b294419e23759b5bde3be6f2fc098c4ec5a150
ajackwere <austinewere59@gmail.com>
2023-08-27 23:01:15 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count, prize=0):
+        self.title = title
+        self._prize = prize
+        self.page_count = page_count
+    
+    @property
+    def pageCount(self):
+        return self.page_count
+    
+    @pageCount.setter
+    def pageCount(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer\n")
+        else:
+            self.page_count = value
+    @property
+    def prize(self):
+        return self._prize
+    
+    @prize.setter
+    def prize(self, value):
+        if value <= 0:
+            raise ValueError("prize must be a positive number")
+        self._prize = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+Book1 = Book("Learn Python in Weeks", 32, 100)
+Book2 = Book("Learn JavaScript", 122, 100)
+
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.size = size
+        self.brand = brand
+    @property
+    def sizeSet(self):
+        return self.size
+    
+    @sizeSet.setter
+    def sizeSet(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer\n")
+        else:
+            self.size = value
+    def cobble(self):
+        print("says that the shoe has been repaired.")
+    def cobble_makes_new(self, condition):
+        self.condition = condition
+        if self.condition == "New":
+            print("New")
+    
+
